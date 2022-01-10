c885ce20e48e594381e0354df6cbc1703f1001f8
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-01-10 13:20:23 -0300

    Constants.py Implementation

---
+++ constants.py
@@ -0,0 +1,50 @@
+# Names
+OUTPUT = 'output'
+MESSAGES = 'messages'
+
+# Symbols
+VOID = ""
+PI = "pi"
+DOT = '.'
+CARET ='^'
+PLUS = '+'
+CLEAR = 'C'
+MINUS = '-'
+EQUAL = '='
+DIVIDE = '/'
+MULTIPLY = '*'
+PERCETAGE = '%'
+ABSOLUTE = "|x|"
+PLUS_MINUS = "±"
+SQUARE_ROOT = "root"
+INFINITY = "infinity"
+FACTORIAL = "factorial"
+PARENTHESES = "parentheses"
+
+# Numbers
+TWO ='2'
+ONE = '1'
+SIX = '6'
+NINE ='9'
+ZERO = '0'
+FOUR = '4'
+FIVE = '5'
+THREE = '3'
+SEVEN = '7'
+EIGHT = '8'
+
+# Oparation names
+SUM = 'sum' 
+DIF = 'dif' 
+EQS = 'equals' 
+DIV = 'divide' 
+MUL = 'multiply'
+
+# lists
+SYMBOLS = [ DOT, CARET, PLUS, CLEAR, MINUS, EQUALS, DIVIDE, MULTIPLY, PERCETAGE, 
+            ABSOLUTE, PLUS_MINUS, SQUARE_ROOT, FACTORIAL, PARENTHESES   ]
+
+NUMBERS = [ ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE  ]
+
+# Messages
+ZERO_DIV_ERR = "Error: Division by zero"
