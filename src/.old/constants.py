de523cb230bbd8f657cfd2201b9f43ae6a6bc10f
Kelthon <kelthonbalbino@gmail.com>
2023-07-14 19:54:53 -0300

    refact: old files moved to old version folder

---
+++ src/.old/constants.py
@@ -0,0 +1,73 @@
+# Names
+TITLE="Calculadora"
+NAME = "CalcPy"
+OUTPUT = "output"
+MESSAGES = "messages"
+
+# Symbols
+VOID = ""
+PI = "pi"
+DOT = '.'
+CARET ='^'
+PLUS = '+'
+CLEAR = 'C'
+MINUS = '-'
+EQUAL = '='
+DIVIDE = '/'
+MULTIPLY = '*'
+PERCETAGE = '%'
+ABSOLUTE = "|x|"
+PLUS_MINUS = "±"
+SQUARE_ROOT = "root"
+INFINITY = "infinity"
+FACTORIAL = "factorial"
+PARENTHESES = "parentheses"
+
+# Numbers
+TWO ='2'
+ONE = '1'
+SIX = '6'
+NINE ='9'
+ZERO = '0'
+FOUR = '4'
+FIVE = '5'
+THREE = '3'
+SEVEN = '7'
+EIGHT = '8'
+
+# Oparation names
+SUM = 'sum' 
+DIF = 'dif' 
+EQS = 'equals' 
+DIV = 'divide' 
+MUL = 'multiply'
+POW = 'pow'
+SQRT = 'root'
+PERC = 'percent'
+
+# Colors
+DISPLAY = "#F4FBF4"
+PRIMARY = "#FF3333"
+SECONDARY = "#FF3333"
+TERTIARY = "#FF9933"
+WHITE = "#F4F4FB"
+GRAY = "#E5E5EC"
+GRAY_FOCUS = "#D7D7DC"
+BLACK = "#000000"
+
+# lists
+SYMBOLS = [ CARET, PLUS, CLEAR, MINUS, EQUAL, DIVIDE, MULTIPLY, PERCETAGE, 
+            ABSOLUTE, PLUS_MINUS, SQUARE_ROOT, FACTORIAL, PARENTHESES   ]
+
+NUMBERS = [ ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE  ]
+
+BASIC_OPERATIONS = { SUM:PLUS, DIF:MINUS, MUL:MULTIPLY, DIV:DIVIDE }
+
+# Messages
+ERR = "Error"
+ZERO_DIV_ERR = "Error: Division by zero"
+NEGATIVE_SQRT_ERR = "Error: negative square root"
+
+# Config
+SIZE_BTN = (10, 3)
+FONT = "Seven Segment"
